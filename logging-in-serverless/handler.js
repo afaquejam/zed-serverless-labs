dd3a297c091ba5361c5b48bf5c58135ec7f8bcf5
Afaque H <afaque.hussain@nordcloud.com>
2020-03-21 18:01:41 +0200

    Serverless logging: Add Dynamodb Streams
    Check if co-relationId propagates.

--- logging-in-serverless/handler.js
+++ logging-in-serverless/handler.js
@@ -4,11 +4,12 @@ const { MEASUREMENTS_TABLE } = process.env;
 const AWS = require('aws-sdk');
 const Log = require('@dazn/lambda-powertools-logger');
 const wrap = require('@dazn/lambda-powertools-pattern-basic');
+const DynamoDB = require('@dazn/lambda-powertools-dynamodb-client');
 
-module.exports.getMeasurements = wrap(async event => {
-  Log.debug('Received the following event:', event)
+const getRandomData = () => Math.round(Math.random() * 100);
 
-  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+module.exports.getMeasurements = wrap(async (event) => {
+  Log.debug('Received the following event:', event);
 
   const params = {
     TableName: MEASUREMENTS_TABLE,
@@ -18,18 +19,63 @@ module.exports.getMeasurements = wrap(async event => {
   let dynamoDbResponse;
 
   try {
-    dynamoDbResponse = await dynamoDb.scan(params).promise();
+    dynamoDbResponse = await DynamoDB.scan(params).promise();
     Log.info('The dynamodb response is', dynamoDbResponse);
 
     return {
       statusCode: 200,
       body: JSON.stringify(dynamoDbResponse),
-    }
+    };
   } catch (error) {
     Log.error('Failed to fetch data from dynamodb', error);
     return {
       statusCode: 400,
       body: JSON.stringify(error),
-    }
+    };
   }
+});
+
+module.exports.storeMeasurements = wrap(async (event) => {
+  Log.info(event);
+
+  const { deviceId } = JSON.parse(event.body);
+
+  const params = {
+    TableName: MEASUREMENTS_TABLE,
+    Item: {
+      deviceId,
+      timestamp: Date.now(),
+      temperature: getRandomData(),
+      lightIntensity: getRandomData(),
+      soundIntensity: getRandomData(),
+    },
+  };
+
+  Log.debug('The dynamodb put parameters are:', params);
+
+  try {
+    await DynamoDB.put(params).promise();
+    Log.info('The dynamoDb put operation was successful.');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Measurement stored successfully!',
+      }),
+    };
+  } catch (error) {
+    Log.error('Failed to store data in dynamodb', error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error),
+    };
+  }
+});
+
+module.exports.computeAggregate = wrap(async (event) => {
+  Log.info(event);
+
+  return {
+    statusCode: 200,
+  };
 });
